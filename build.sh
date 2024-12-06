bb5ba88a659b8727c5d0d2a8754b5ccad8da7b23
ftkey <ftkey@qq.com>
2024-12-06 13:56:06 +0800

    测试IB

--- build.sh
+++ build.sh
@@ -8,16 +8,16 @@ PROFILE="jdcloud_re-cs-07"
 
 PACKAGES=""
 
-# Argon 主题
-PACKAGES="$PACKAGES luci-theme-argon luci-i18n-argon-config-zh-cn"
+# # Argon 主题
+# PACKAGES="$PACKAGES luci-theme-argon luci-i18n-argon-config-zh-cn"
 
-# Samba 网络共享
-PACKAGES="$PACKAGES luci-i18n-samba4-zh-cn"
-# Zerotier
-PACKAGES="$PACKAGES luci-i18n-zerotier-zh-cn"
+# # Samba 网络共享
+# PACKAGES="$PACKAGES luci-i18n-samba4-zh-cn"
+# # Zerotier
+# PACKAGES="$PACKAGES luci-i18n-zerotier-zh-cn"
 
-# 界面翻译补全
-PACKAGES="$PACKAGES luci-i18n-opkg-zh-cn luci-i18n-base-zh-cn luci-i18n-firewall-zh-cn"
+# # 界面翻译补全
+# PACKAGES="$PACKAGES luci-i18n-opkg-zh-cn luci-i18n-base-zh-cn luci-i18n-firewall-zh-cn"
 
 # 一些自定义文件
 FILES="files"
